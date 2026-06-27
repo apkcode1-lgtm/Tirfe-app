ea2206208d115ce073eaf844a0a7f0265e6a7f24
apkcode1-lgtm <apkcode1@gmail.com>
2026-06-27 09:58:07 +0300

    Update Firebase configuration settings
    Updated firebase configuration with new API keys and project details.

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,13 @@
 
 ﻿const firebaseConfig = { databaseURL: "https://tirfe-app-v2-300c2-default-rtdb.firebaseio.com/" };
 firebase.initializeApp(firebaseConfig);
+const firebaseConfig = {
+  apiKey: "AIzaSyBgXU6N4cMV2q-d3XeFzvgFT98gJ1GM7Ws",
+  authDomain: "tirfe-app.firebaseapp.com",
+  databaseURL: "https://tirfe-app-default-rtdb.firebaseio.com",
+  projectId: "tirfe-app",
+  storageBucket: "tirfe-app.firebasestorage.app",
+  messagingSenderId: "228622358915",
+  appId: "1:228622358915:web:c9ff3039a6d6cf66613eb6"
+};
 const db = firebase.database();
