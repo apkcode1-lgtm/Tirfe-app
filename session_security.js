6aaf263e8f3f1947396c4491d1cacf35fe0d60fa
apkcode1-lgtm <apkcode1@gmail.com>
2026-07-16 16:05:39 +0300

    Change fetch call from 'owner' to 'shop'

--- session_security.js
+++ session_security.js
@@ -72,7 +72,7 @@ function checkAutomaticLogin() {
             currentUserRole = session.role; // ሮሉን ማረጋገጥ
             
             if(isLoginPage) {
-            await fetchAndRenderSecureHTML('owner');
+            await fetchAndRenderSecureHTML('shop');
             } else {
                 // መፍትሄ፡ አሁን ሱቁ ገፅ ላይ (shop.html) ከሆነ አፑን እና በተኖቹን ማስነሳት (Launch) አለበት!
                 if(typeof launchApp === "function") {
