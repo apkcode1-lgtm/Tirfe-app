0c29c0bee26e206c7c7f6f5410d7f74a6b346346
apkcode1-lgtm <apkcode1@gmail.com>
2026-07-22 22:56:54 +0300

    Refactor service worker caching and fetch logic

--- sw.js
+++ sw.js
@@ -2,21 +2,18 @@ const CACHE_NAME = 'muller-delivery-v1';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/manifest.json'
-  // እዚህ ጋር ሌሎች CSS፣ JS ወይም ምስል ፋይሎችን መጨመር ትችላለህ
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
 ];
 
-// 1. Service Worker ሲጫን (Install ሲደረግ)
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// 2. የድሮ ፋይሎችን ለማጥፋት (Activate ሲደረግ)
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
@@ -31,12 +28,8 @@ self.addEventListener('activate', event => {
   );
 });
 
-// 3. ኢንተርኔት በማይኖርበት ጊዜ (Fetch)
 self.addEventListener('fetch', event => {
   event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
-    })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
-
